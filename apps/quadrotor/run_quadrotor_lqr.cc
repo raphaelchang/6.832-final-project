78f097553076cccec18cffd691d1bcf55f30990b
raphaelchang <raphaelchang1227@gmail.com>
2018-05-13 05:07:37 -0400

    Start minimum snap trajectory

--- apps/quadrotor/run_quadrotor_lqr.cc
+++ apps/quadrotor/run_quadrotor_lqr.cc
@@ -40,6 +40,74 @@ namespace examples {
 namespace quadrotor {
 namespace {
 
+PiecewisePolynomial<double> MinimumSnap(const Eigen::VectorXd &breaks, const Eigen::MatrixXd &knots, int order, int k_r, int k_phi)
+{
+    double mu_r = 1.0;
+    double mu_phi = 1.0;
+    MatrixXd poly_r = MatrixXd::Ones(order + 1, 1);
+    MatrixXd poly_phi = MatrixXd::Ones(order + 1, 1);
+    for (int i = k_r; i <= order; i++)
+    {
+        int term = 1;
+        for (int j = 0; j < k_r; j++)
+        {
+            term = term * (i - j);
+        }
+        poly_r(i - k_r, 0) = (double)term;
+    }
+    for (int i = k_phi; i <= order; i++)
+    {
+        int term = 1;
+        for (int j = 0; j < k_phi; j++)
+        {
+            term = term * (i - j);
+        }
+        poly_phi(i - k_phi, 0) = (double)term;
+    }
+    MatrixXd Q(4 * (order + 1) * breaks.size(), 4 * (order + 1) * breaks.size());
+    for (int i = 0; i < breaks.size() - 1; i++)
+    {
+        MatrixXd Q_r = MatrixXd::Zero(order + 1, order + 1);
+        for (int j = 0; j <= order - k_r; j++)
+        {
+            for (int k = j; k <= order - k_r; k++)
+            {
+                int order_t_r = order - k_r - j + order - k_r - k;
+                if (j == k)
+                {
+                    Q_r(j, k) = poly_r(j) * poly_r(j) / (order_t_r + 1) * (std::pow(breaks(i + 1), order_t_r + 1) - std::pow(breaks(i), order_t_r + 1));
+                }
+                else
+                {
+                    Q_r(j, k) = 2 * poly_r(j) * poly_r(k) / (order_t_r + 1) * (std::pow(breaks(i + 1), order_t_r + 1) - std::pow(breaks(i), order_t_r + 1));
+                }
+            }
+        }
+        MatrixXd Q_phi = MatrixXd::Zero(order + 1, order + 1);
+        for (int j = 0; j <= order - k_phi; j++)
+        {
+            for (int k = j; k <= order - k_phi; k++)
+            {
+                int order_t_r = order - k_phi - j + order - k_phi - k;
+                if (j == k)
+                {
+                    Q_phi(j, k) = poly_phi(j) * poly_phi(j) / (order_t_r + 1) * (std::pow(breaks(i + 1), order_t_r + 1) - std::pow(breaks(i), order_t_r + 1));
+                }
+                else
+                {
+                    Q_phi(j, k) = 2 * poly_phi(j) * poly_phi(k) / (order_t_r + 1) * (std::pow(breaks(i + 1), order_t_r + 1) - std::pow(breaks(i), order_t_r + 1));
+                }
+            }
+        }
+        Q.block(i * 4 * (order + 1), i * 4 * (order + 1), order + 1, order + 1) = mu_r * Q_r;
+        Q.block(i * 4 * (order + 1) + order + 1, i * 4 * (order + 1) + order + 1, order + 1, order + 1) = mu_r * Q_r;
+        Q.block(i * 4 * (order + 1) + 2 * (order + 1), i * 4 * (order + 1) + 2 * (order + 1), order + 1, order + 1) = mu_r * Q_r;
+        Q.block(i * 4 * (order + 1) + 3 * (order + 1), i * 3 * (order + 1) + 2 * (order + 1), order + 1, order + 1) = mu_phi * Q_phi;
+    }
+    std::cout << Q << std::endl;
+    return PiecewisePolynomial<double>::Cubic(breaks, knots);
+}
+
 int do_main() {
   lcm::DrakeLcm lcm;
 
@@ -56,16 +124,22 @@ int do_main() {
 
   auto quadrotor = builder.AddSystem<QuadrotorPlant<double>>();
   quadrotor->set_name("quadrotor");
-  auto controller = builder.AddSystem(std::make_unique<TrajectoryController<double>>(*quadrotor, 1.0, 2.0, 1.0, 0.8));
+  auto controller = builder.AddSystem(std::make_unique<TrajectoryController<double>>(*quadrotor, 1.5, 3.0, 1.0, 0.8));
   controller->set_name("controller");
   auto visualizer =
       builder.AddSystem<drake::systems::DrakeVisualizer>(*tree, &lcm);
   visualizer->set_name("visualizer");
-  Vector2d breaks;
-  breaks << 0.0, 10.0;
-  MatrixXd knots(4, 2);
-  knots << 1, 1, 3, 3, 1.0, 1.0, 0.5, 0.5;
-  const PiecewisePolynomial<double> traj = PiecewisePolynomial<double>::ZeroOrderHold(breaks, knots);
+  int num_knots = 5;
+  VectorXd breaks(num_knots);
+  breaks << 0.0, 5.0, 10.0, 15.0, 20.0;
+  MatrixXd knots(4, num_knots);
+  Vector4d knot1(3, 3, 1, 0);
+  Vector4d knot2(-3, 3, 1, M_PI / 2.);
+  Vector4d knot3(-3, -3, 1, M_PI);
+  Vector4d knot4(3, -3, 1, 3 * M_PI / 2.);
+  Vector4d knot5(3, 3, 1, 0);
+  knots << knot1, knot2, knot3, knot4, knot5;
+  const PiecewisePolynomial<double> traj = MinimumSnap(breaks, knots, 6, 4, 4);
   auto trajectory = builder.AddSystem<systems::TrajectorySource>(traj);
   trajectory->set_name("trajectory");
 
@@ -86,7 +160,7 @@ int do_main() {
 
   simulator.Initialize();
   simulator.set_target_realtime_rate(FLAGS_simulation_real_time_rate);
-  simulator.StepTo(traj.end_time());
+  simulator.StepTo(traj.end_time() + 5.0);
 
   return 0;
 }
